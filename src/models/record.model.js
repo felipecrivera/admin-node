373b89a0d4f5511a0cd573acc9bf460e8127a045
felipecrivera <felipecrivera.0987@gmail.com>
2024-02-13 08:29:00 -0600

    remove required fields

--- src/models/record.model.js
+++ src/models/record.model.js
@@ -4,27 +4,21 @@ const recordSchema = mongoose.Schema(
   {
     activationDate: {
       type: String,
-      required: true,
     },
     campaign: {
       type: String,
-      required: true,
     },
     company: {
       type: String,
-      required: true,
     },
     firstName: {
       type: String,
-      required: true,
     },
     lastName: {
       type: String,
-      required: true,
     },
     title: {
       type: String,
-      required: true,
     },
     email: {
       type: String,
@@ -32,39 +26,30 @@ const recordSchema = mongoose.Schema(
     },
     phone: {
       type: String,
-      required: true,
     },
     address: {
       type: String,
-      required: true,
     },
     city: {
       type: String,
-      required: true,
     },
     state: {
       type: String,
-      required: true,
     },
     zipCode: {
       type: String,
-      required: true,
     },
     outCome: {
       type: String,
-      required: true,
     },
     bookingDate: {
       type: String,
-      reuired: false,
     },
     bookingTime: {
       type: String,
-      reuired: false,
     },
     notes: {
       type: String,
-      reuired: false,
     },
   },
   { timestamps: true }
